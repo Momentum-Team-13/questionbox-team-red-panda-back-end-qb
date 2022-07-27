1d9a4bbb3804d1a748dfe4dbf6cc7b92738a2a05
John Parrish <Johnwillparrish3@gmail.com>
2022-07-27 16:46:40 -0400

    question list view/url set up

--- question_box/models.py
+++ question_box/models.py
@@ -22,7 +22,7 @@ class Question(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='questions')
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    save = models.ManyToManyField(User, related_name='save', default=None, blank=True)
+    favorited_by = models.ManyToManyField(User, related_name='favorite_questions')
 
     def __str__(self):
         return self.title
