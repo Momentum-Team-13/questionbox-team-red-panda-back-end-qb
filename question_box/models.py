3708648fe8e0882c1d1d797eb41a480512051d37
Caelll <cael.blackmon1@gmail.com>
2022-07-30 12:05:37 -0400

    Finished our answer line

--- question_box/models.py
+++ question_box/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+import question_box
+
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(db_index=True, auto_now_add=True)
@@ -21,8 +23,17 @@ class User(AbstractUser):
 class Question(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='questions')
     title = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    description = models.CharField(max_length=500)
     favorited_by = models.ManyToManyField(User, related_name='favorite_questions', blank=True)
 
     def __str__(self):
         return self.title
+
+class Answer(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='answers')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answered_question')
+    description = models.CharField(max_length=500)
+    favorited_by = models.ManyToManyField(User, related_name='favorite_answers', blank=True)
+
+    def __str__(self):
+        return self.description
